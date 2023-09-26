2eee1c40295e0464f388a3edc230f4cc5964fb11
by-LindaH <117244139+Sakeyasu@users.noreply.github.com>
2023-09-26 11:53:49 +0900

    srcのファイル構造修正

---
+++ src(実装)/category.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<h1>Category</h1>
+
+<button>北海道地方</button>
+<button>東北地方</button>
+<button>関東地方</button>
+<button>中部地方</button>
+<button>近畿地方</button>
+<button>中国地方</button>
+<button>四国地方</button>
+<button>九州地方</button>
+<button>海外</button>
