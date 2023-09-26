88dbf5f98aaacc1e8225fb6332c62e29e0a6dead
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-09-26 12:25:33 +0900

    現段階最新9/26/12:25

---
+++ src(実装)/AnotherSky/another.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/home.css"> 
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/login.php/'" value=""><h2 style="text-align: right;">log in&nbsp;&nbsp;</h2></a>
+    &nbsp; &nbsp;
+    <a type="button" onclick="location.href='http://localhost/php/kaihatu/home.php/'" value=""><h2>side</h2></a>
+<div style="text-align: center;">
+<div class="Header">
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/home.php/'" value=""><font face="serif">home&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/hometown.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;hometown&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/another.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;anoher&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/category.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;category&nbsp;&nbsp;&nbsp;</font></a>
+    </div>
+</div>
+<div><hr /></div>
+<div>
+  <h2>
+    <div style="text-align: center;">
+      <span style="font-size: 1.5em;">
+        <font face="serif">Another Sky</font>
+      </span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-size: 1.5em;">
+        <font face="serif"><br /></font>
+      </span>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif"><span style="font-size: 36px;">[1]</span></font>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif"><span style="font-size: 36px;">[2]</span></font>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif"><span style="font-size: 36px;">[3]</span></font>
+    </div>
+    <div style="text-align: center;"><br /></div>
+    <div style="text-align: center;"><hr /></div>
+    <div style="text-align: center;">
+      <font face="serif">@Another Sky</font>
+    </div>
+  </h2>
+</div>
+
+</body>
+</html>
---
+++ src(実装)/AnotherSky/menu.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/home.css"> 
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/login.php/'" value=""><h2 style="text-align: right;">log in&nbsp;&nbsp;</h2></a>
+    &nbsp; &nbsp;
+    <a type="button" onclick="location.href='http://localhost/php/kaihatu/home.php/'" value=""><h2>side</h2></a>
+<div style="text-align: center;">
+<div class="Header">
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/home.php/'" value=""><font face="serif">home&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/hometown.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;hometown&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/another.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;anoher&nbsp;&nbsp;&nbsp;</font></a>
+<a type="button" onclick="location.href='http://localhost/php/kaihatu/category.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;category&nbsp;&nbsp;&nbsp;</font></a>
+    </div>
+</div>
+<div><hr /></div>
+<h2>
+    &nbsp; &nbsp;
+  <font face="serif">忘れられない空がある</font>
+</h2>
+<h1>
+    &nbsp; &nbsp;
+  <font face="serif">Another&nbsp;</font>
+  Sky
+</h1>
+<h4> &nbsp; &nbsp;みんなの第二の故郷を知り、あなたの第二の故郷を広め、</h4>
+<h4 style="text-align: justify;">
+   &nbsp; &nbsp;多くの価値観を共有しましょう
+</h4>
+<div>
+  <h2>
+    <hr />
+    <div style="text-align: center;">
+      <span style="font-size: 1.5em;">
+        <font face="serif"><br /></font>
+      </span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-size: 1.5em;">
+        <font face="serif">Another Sky</font>
+      </span>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">[1]</text></svg>
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">[2]</text></svg>
+            <div class="card-body">
+              <p class="card-text">Another Skyの魅力！
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">[3]</text></svg>
+            <div class="card-body">
+              <p class="card-text">Another Skyの魅力！
+              </p>
+              <div class="d-flex justify-content-between align-items-center">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-size: 1.5em;">
+        <font face="serif"><br /></font>
+      </span>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif">
+        <span style="font-size: 36px;"><br /></span>
+      </font>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif">
+        <span style="font-size: 36px;"><br /></span>
+      </font>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif">
+        <span style="font-size: 36px;"><br /></span>
+      </font>
+    </div>
+    <div style="text-align: center;">
+    <a type="button" onclick="location.href='http://localhost/php/kaihatu/another.php/'" value=""><font face="serif" size="3">more view</font></a>
+    </div>
+    <div style="text-align: center;">
+      <font face="serif" size="3"><br /></font>
+    </div>
+    <div style="text-align: center;">
+      <hr />
+      <span style="font-family: serif; font-size: 36px;">Hometown</span>
+      <br />
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;">title</span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;">title</span>
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;">title</span>
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+      <span style="font-family: serif; font-size: 36px;"><br /></span>
+    </div>
+    <div style="text-align: center;">
+    <a type="button" onclick="location.href='http://localhost/php/kaihatu/hometown.php/'" value=""><span style="font-family: serif; font-size: medium;">more view</span></a>
+    </div>
+    <div style="text-align: center;">
+      <hr />
+      <font face="serif">@Another Sky</font>
+    </div>
+  </h2>
+</div>
+
+</body>
+</html>
