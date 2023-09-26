1e72f16f7bf763363ca9b61c0cdaae66c5e1ae5f
by-LindaH <117244139+Sakeyasu@users.noreply.github.com>
2023-09-26 09:39:41 +0900

    Create demo-sakemi.php

---
+++ implemention(実装)/AnotherSky/src/demo-sakemi.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+<h1>Another Sky</h1>
+<h3>通常版</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/G-Hu9ytV-1Y?si=8l7JNv5ciQe9w2ad" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+<h3>おすすめ表示なし&自動再生版</h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/G-Hu9ytV-1Y?si=8l7JNv5ciQe9w2ad?&autoplay=1&modestbranding=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
