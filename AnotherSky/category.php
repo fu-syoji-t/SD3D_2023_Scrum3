b8223238e87d81b1bdf09bdad6881e45ca779d61
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-09-29 11:51:25 +0900

    9/2911:51更新

--- AnotherSky/category.php
+++ AnotherSky/category.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/home.css"> 
+    <link rel="stylesheet" href="../home.css"> 
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-<a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/login.php/'" value=""><h2 style="text-align: right;">log in&nbsp;&nbsp;</h2></a>
+<a onclick="location.href='../AnotherSky/login.php'" value=""><h2 style="text-align: right;">log in&nbsp;&nbsp;</h2></a>
     &nbsp; &nbsp;
-    <a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/home.php/'" value=""><h2>side</h2></a>
+    <a type="button" onclick="location.href='../AnotherSky/home.php'" value=""><h2>side</h2></a>
 <div style="text-align: center;">
 <div class="Header">
-<a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/home.php/'" value=""><font face="serif">home&nbsp;&nbsp;&nbsp;</font></a>
-<a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/hometown.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;hometown&nbsp;&nbsp;&nbsp;</font></a>
-<a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/another.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;anoher&nbsp;&nbsp;&nbsp;</font></a>
-<a type="button" onclick="location.href='http://localhost/web/SD3D_2023_Scrum3/AnotherSky/category.php/'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;category&nbsp;&nbsp;&nbsp;</font></a>
+<a onclick="location.href='../AnotherSky/home.php'" value=""><font face="serif">home&nbsp;&nbsp;&nbsp;</font></a>
+<a onclick="location.href='../AnotherSky/hometown.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;hometown&nbsp;&nbsp;&nbsp;</font></a>
+<a onclick="location.href='../AnotherSky/another.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;anoher&nbsp;&nbsp;&nbsp;</font></a>
+<a onclick="location.href='../AnotherSky/category.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;category&nbsp;&nbsp;&nbsp;</font></a>
     </div>
 </div>
 <div><hr /></div>
@@ -32,15 +32,15 @@
       <span style="font-size: 36px; font-family: serif;"><br /></span>
     </div>
     <div style="text-align: center;">
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categoryho.php/'" value=""><span style="font-size: 36px; font-family: serif;">北海道&nbsp;</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categoryto.php/'" value=""><span style="font-family: serif; font-size: 36px;">東北</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categoryknt.php/'" value=""><span style="font-size: 36px; font-family: serif;">&nbsp;関東&nbsp; &nbsp;</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categorytu.php/'" value=""><span style="font-family: serif; font-size: 36px;">中部</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categoryknk.php/'" value=""><span style="font-size: 36px; font-family: serif;">&nbsp;近畿&nbsp; &nbsp;</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categorytuk.php/'" value=""><span style="font-family: serif; font-size: 36px;">中国</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categorysko.php/'" value=""><span style="font-size: 36px; font-family: serif;">四国 </span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categorykys.php/'" value=""><span style="font-family: serif; font-size: 36px;">九州</span></a>
-    <a type="button" onclick="location.href='http://localhost/php/AnotherSky/categoryabroad.php/'" value=""><span style="font-family: serif; font-size: 36px;">海外   </span></a>
+    <a onclick="location.href='../AnotherSky/categoryho.php'" value=""><span style="font-size: 36px; font-family: serif;">北海道&nbsp;</span></a>
+    <a onclick="location.href='../AnotherSky/categoryto.php'" value=""><span style="font-family: serif; font-size: 36px;">東北</span></a>
+    <a onclick="location.href='../AnotherSky/categoryknt.php'" value=""><span style="font-size: 36px; font-family: serif;">&nbsp;関東&nbsp; &nbsp;</span></a>
+    <a onclick="location.href='../AnotherSky/categorytu.php'" value=""><span style="font-family: serif; font-size: 36px;">中部</span></a>
+    <a onclick="location.href='../AnotherSky/categoryknk.php'" value=""><span style="font-size: 36px; font-family: serif;">&nbsp;近畿&nbsp; &nbsp;</span></a>
+    <a onclick="location.href='../AnotherSky/categorytuk.php'" value=""><span style="font-family: serif; font-size: 36px;">中国</span></a>
+    <a onclick="location.href='../AnotherSky/categorysko.php'" value=""><span style="font-size: 36px; font-family: serif;">四国 </span></a>
+    <a onclick="location.href='../AnotherSky/categorykys.php'" value=""><span style="font-family: serif; font-size: 36px;">九州</span></a>
+    <a onclick="location.href='../AnotherSky/categoryabroad.php'" value=""><span style="font-family: serif; font-size: 36px;">海外   </span></a>
     </div>
   </h2>
   <h2>
