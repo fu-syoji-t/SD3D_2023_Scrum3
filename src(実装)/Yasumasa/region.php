8eaab67110ef66a32b67c15d599b604c57c6d852
by-LindaH <117244139+Sakeyasu@users.noreply.github.com>
2023-09-29 12:45:20 +0900

    region更新 404追加

--- src(実装)/Yasumasa/region.php
+++ src(実装)/Yasumasa/region.php
@@ -1,7 +1,34 @@
 <?php
-
+//echo $_GET['region'];
 ?>
 <html>
 <!-- 地域別投稿表示画面(バック) -->
-
+    <head>
+        <title>地域別投稿</title>
+    </head>
+    <body>
+        <?php
+        if (isset($_GET["parameter"])) {
+            $selectedParameter = $_GET["parameter"];
+            
+            // パラメータに基づいて異なるコンテンツを表示
+            switch ($selectedParameter) {
+                case "1":
+                    echo "<h1>北海道地方</h1>";
+                    break;
+                case "2":
+                    echo "<h1>東北地方</h1>";
+                    break;
+                case "3":
+                    echo "<h1>関東地方</h1>";
+                    break;
+                default:
+                    echo "<h1>404 Error<br>このページは存在しません</h1>";
+                    echo "<img src='../image/404.jpg' alt='404 Eroor'>";
+            }
+        } else {
+            echo "<h1>パラメータが送信されませんでした</h1>";
+        }
+        ?>
+    </body>
 </html>
--- src(実装)/Yasumasa/region_select.php
+++ src(実装)/Yasumasa/region_select.php
@@ -1,4 +1,16 @@
 <?php
+// echo "<form action='region.php' name=region method='post'>";
+
+// echo "<button type='submit' name=region value='1'>北海道地方</button>";
+// echo "<button type='submit' name=region value='2'>東北地方</button>";
+// echo "<button type='submit' name=region value='3'>関東地方</button>";
+// echo "<button type='submit' name=region value='4'>中部地方</button>";
+// echo "<button type='submit' name=region value='5'>近畿地方</button>";
+// echo "<button type='submit' name=region value='6'>中国地方</button>";
+// echo "<button type='submit' name=region value='7'>四国地方</button>";
+// echo "<button type='submit' name=region value='8'>九州地方</button>";
+// echo "<button type='submit' name=region value='9'>海外</button>";
+// echo "</form>";
 ?>
 <!DOCTYPE html>
 <head>
@@ -8,16 +20,38 @@
 <body>
     <h1>Category</h1>
     <!-- 地域選択画面(バック) -->
-    <form action="region.php" method="post">
-        <button type="submit" value="1">北海道地方</button>
-        <button type="submit" value="2">東北地方</button>
-        <button type="submit" value="3">関東地方</button>
-        <button type="submit" value="4">中部地方</button>
-        <button type="submit" value="5">近畿地方</button>
-        <button type="submit" value="6">中国地方</button>
-        <button type="submit" value="7">四国地方</button>
-        <button type="submit" value="8">九州地方</button>
-        <button type="submit" value="9">海外</button>
-    </form>
+    <!--<form action="region.php" name=region method="post">
+        <button type="submit" name=region value="1">北海道地方</button>
+        <button type="submit" name=region value="2">東北地方</button>
+        <button type="submit" name=region value="3">関東地方</button>
+        <button type="submit" name=region value="4">中部地方</button>
+        <button type="submit" name=region value="5">近畿地方</button>
+        <button type="submit" name=region value="6">中国地方</button>
+        <button type="submit" name=region value="7">四国地方</button>
+        <button type="submit" name=region value="8">九州地方</button>
+        <button type="submit" name=region value="9">海外</button>
+    </form>-->
+
+    <button id="option1" data-parameter="1">北海道地方</button>
+    <button id="option2" data-parameter="2">東北地方</button>
+    <button id="option3" data-parameter="3">関東地方</button>
+    
+    <script>
+        // ボタンがクリックされたときの処理
+        document.getElementById("option1").addEventListener("click", function() {
+            redirectToNextPage("hkd");
+        });
+        document.getElementById("option2").addEventListener("click", function() {
+            redirectToNextPage("thk");
+        });
+        document.getElementById("option3").addEventListener("click", function() {
+            redirectToNextPage("knt");
+        });
+
+        // 次のページに遷移する関数
+        function redirectToNextPage(parameter) {
+            window.location.href = "region.php?parameter=" + parameter;
+        }
+    </script>
 </body>
 </html>
