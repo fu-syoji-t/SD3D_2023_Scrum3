5da558758ca72f77f270dbd73d662cd2ae74a4f3
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-10-02 10:53:10 +0900

    フロントcss、リンク修正更新10/02

--- AnotherSky/categoryabroad.php
+++ AnotherSky/categoryabroad.php
@@ -5,20 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="../home.css"> 
+    <link rel="stylesheet" href="../css/homea.css">
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <a onclick="location.href='../AnotherSky/login.php'" value=""><h2 style="text-align: right;">log in&nbsp;&nbsp;</h2></a>
     &nbsp; &nbsp;
     <a onclick="location.href='../AnotherSky/home.php'" value=""><h2>side</h2></a>
-<div style="text-align: center;">
-<div class="Header">
-<a onclick="location.href='../AnotherSky/home.php'" value=""><font face="serif">home&nbsp;&nbsp;&nbsp;</font></a>
-<a onclick="location.href='../AnotherSky/hometown.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;hometown&nbsp;&nbsp;&nbsp;</font></a>
-<a onclick="location.href='../AnotherSky/another.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;anoher&nbsp;&nbsp;&nbsp;</font></a>
-<a onclick="location.href='../AnotherSky/category.php'" value=""><font face="serif">&nbsp;&nbsp;&nbsp;category&nbsp;&nbsp;&nbsp;</font></a>
-    </div>
+    <?php  require_once 'header.php' ?>
 </div>
 <div><hr /></div>
 <div>
---
+++ AnotherSky/home1.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>HTML+CSS</title>
+    <style>
+    </style>
+  </head>
+  <body>
+
+    <?php  require_once 'header.php' ?>
+
+    <h1>BLOG</h1>
+    <div class="box">
+      <h2>
+        <a href="#">大分　湯布院</a>
+        <span class="date">(2023.09.29)</span>
+      </h2>
+      <p class="box_img">
+        <img src="img/desktop.JPG" alt="Thumbnail" width="150" height="150">
+      </p>
+
+      <div class="box_sentence">
+        <p>
+          私のアナザースカイは大分の湯布院です。心が乱れている時によく行ってます<br>
+          ここの金賞コロッケを食べてガラスの森に行くのがいつものルーティンです<br>
+          今までもこれからも通いたい私のアナザースカイです。ぜひ詳細をご覧ください…
+          <a href="#">続きを読む</a>
+        </p>
+      </div>
+    </div>
+    <div class="box">
+      <h2>
+        <a href="#">大分　湯布院</a>
+        <span class="date">(2023.09.29)</span>
+      </h2>
+      <p class="box_img">
+        <img src="img/desktop.JPG" alt="Thumbnail" width="150" height="150">
+      </p>
+
+      <div class="box_sentence">
+        <p>
+          私のアナザースカイは大分の湯布院です。心が乱れている時によく行ってます<br>
+          ここの金賞コロッケを食べてガラスの森に行くのがいつものルーティンです<br>
+          今までもこれからも通いたい私のアナザースカイです。ぜひ詳細をご覧ください…
+          <a href="#">続きを読む</a>
+        </p>
+      </div>
+    </div>
+    <div class="box">
+      <h2>
+        <a href="#">大分　湯布院</a>
+        <span class="date">(2023.09.29)</span>
+      </h2>
+      <p class="box_img">
+        <img src="img/desktop.JPG" alt="Thumbnail" width="150" height="150">
+      </p>
+
+      <div class="box_sentence">
+        <p>
+          私のアナザースカイは大分の湯布院です。心が乱れている時によく行ってます<br>
+          ここの金賞コロッケを食べてガラスの森に行くのがいつものルーティンです<br>
+          今までもこれからも通いたい私のアナザースカイです。ぜひ詳細をご覧ください…
+          <a href="#">続きを読む</a>
+        </p>
+      </div>
+    </div>
+    <p class="more"><a href="#">read more...</a></p>
+  </body>
+</html>
+
