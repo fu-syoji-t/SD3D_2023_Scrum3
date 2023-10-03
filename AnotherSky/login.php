b624a4a7ae0b464f84368860a5a58ab42e353f19
nanndemiii <2101160@s.asojuku.ac.jp>
2023-10-03 11:23:05 +0900

    Update login.php

--- AnotherSky/login.php
+++ AnotherSky/login.php
@@ -6,27 +6,27 @@
     <style>
         /* CSSスタイルで中央配置を指定 */
         .centered-text {
-            text-align: center; 
-            position: absolute; 
-            top: 50%; 
-            left: 50%; 
-            transform: translate(-50%, -50%); 
+            text-align: center; /* テキストを中央に配置 */
+            position: absolute; /* 絶対位置指定 */
+            top: 50%; /* 上から50%の位置に配置 */
+            left: 50%; /* 左から50%の位置に配置 */
+            transform: translate(-50%, -50%); /* 中央に配置 */
         }
 
         .rectangle {
-            width: 100px; 
-            height: 100px; 
-            background-color: #ff0000; 
-            margin-top: 20px; 
+            width: 100px; /* 四角形の幅 */
+            height: 100px; /* 四角形の高さ */
+            background-color: #ffffff; /* 白色の背景 */
+            margin-top: 20px; /* テキストとの間隔 */
         }
     </style>
 </head>
 <body>
     <div class="centered-text">
         <?php
-            echo "ログイン";
+            echo "ここに中央の上に配置したいテキストを入力してください。";
         ?>
-        <div class="rectangle"></div> 
+        <div class="rectangle"></div> <!-- 四角形の追加 -->
     </div>
 </body>
 </html>
