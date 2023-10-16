19c7b80f41417fbd0c3dc6fb73a8079bce794674
by-LindaH <117244139+Sakeyasu@users.noreply.github.com>
2023-10-16 14:59:36 +0900

    投稿編集画面作成

---
+++ src(実装)/post_edit(b).php
@@ -0,0 +1,114 @@
+<?php 
+    require_once "DBManager.php";
+    $get = new DBManager();
+
+    $regions = $get->get_regions();
+
+    require_once "tmp_test.php";
+    $template = new header("post");
+?>
+============================================
+
+<?php
+$post_id = $_GET["post"];
+    $post = $get->get_post($post_id);
+
+    require_once "tmp_test.php";
+    $template = new header("hometown_detail");
+?>
+
+    <input type="button" onclick="history.back()" value="戻る"><br>
+    
+    <?php
+    echo 'post_id : ';
+    echo $post["post_id"].'<br>';
+
+    echo 'datetime : ';
+    echo $post["date"]."<br>";
+
+    echo 'title : ';
+    echo $post["title"]."<br>";
+
+    echo 'region : ';
+    echo $post["region"][0]."<br>";
+
+    echo 'place : ';
+    echo $post["place"]."<br>";
+
+    echo 'youtube : <br>';
+    echo '<div>'.$post["link_path"]."</div><br>";
+    
+    echo 'freespace : <br>';
+    echo $post["text"]."<br>";
+
+?>
+
+
+=============================================
+
+<input type="button" onclick="history.back()" value="戻る"><br><br>
+
+<form action="post(b).php" method="post" enctype="multipart/form-data">
+    title : 
+    <input type="text" name="title" maxlength="30"><br>
+    region : 
+    <select name="region">
+        <?php
+            foreach($regions as $region){
+                echo 
+        '<option value='.$region["region_id"].'>'.$region["name"].'</option>';
+            }
+        ?>
+    </select><br>
+    place : 
+    <input type="text" name="place"><br>
+    youtube : 
+    <textarea name="link" rows="8" cols="50"></textarea><br>
+    freespace : 
+    <textarea name="text"></textarea><br><br>
+
+    画像を選択 : 
+    <input type="file" name="post_image1" accept="image/*"><br>
+    具体的なスポット　　　　　　　 : 
+    <textarea name="sentence1_1"></textarea><br>
+    そこに行くようになったきっかけ : 
+    <textarea name="sentence1_2"></textarea><br>
+    どんな思い出があるのか　　　　 : 
+    <textarea name="sentence1_3"></textarea><br><br>
+
+    画像を選択 : 
+    <input type="file" name="post_image2" accept="image/*"><br>
+    具体的なスポット　　　　　　　 : 
+    <textarea name="sentence2_1"></textarea><br>
+    そこに行くようになったきっかけ : 
+    <textarea name="sentence2_2"></textarea><br>
+    どんな思い出があるのか　　　　 : 
+    <textarea name="sentence2_3"></textarea><br><br>
+    
+    画像を選択 : 
+    <input type="file" name="post_image3" accept="image/*"><br>
+    具体的なスポット　　　　　　　 : 
+    <textarea name="sentence3_1"></textarea><br>
+    そこに行くようになったきっかけ : 
+    <textarea name="sentence3_2"></textarea><br>
+    どんな思い出があるのか　　　　 : 
+    <textarea name="sentence3_3"></textarea><br><br>
+    
+    画像を選択 : 
+    <input type="file" name="post_image4" accept="image/*"><br>
+    具体的なスポット　　　　　　　 : 
+    <textarea name="sentence4_1"></textarea><br>
+    そこに行くようになったきっかけ : 
+    <textarea name="sentence4_2"></textarea><br>
+    どんな思い出があるのか　　　　 : 
+    <textarea name="sentence4_3"></textarea><br><br>
+ 
+    <input type="submit" value="投稿">
+</form>
+
+<div style="color:blueviolet">
+<?php 
+    require_once "tmp_test.php";
+    $template = new template("text1!!!","text2!!","text3!");
+?>
+</div>
