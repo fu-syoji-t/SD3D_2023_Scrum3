6c123611a4708a756098696032372835ddeba8ea
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-11-22 10:10:55 +0900

    up

--- AnotherSky/Category/categorykys.php
+++ AnotherSky/Category/categorykys.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../css/hometown.css">
+    <link rel="stylesheet" href="../css/hometown.css"> 
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
@@ -59,7 +59,7 @@ foreach ($posts as $post) {
 }
 ?>
 
-  <div style="width: 100%; background-color: #bbb">
+<div class="read_more2" style="width: 412px; background-color: #bbb">
     <button type="button" class="more" id="more">read more...</button>
   </div>
     <?php  require_once '../!Mng/footer.php' ?>
