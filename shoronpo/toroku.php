555e3a0924cf31a8e7b467a9a2e95bd1910e5b24
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-11-29 12:44:46 +0900

    up

--- shoronpo/toroku.php
+++ shoronpo/toroku.php
@@ -1,11 +1,12 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=another_sky;charset=utf8', 'root', '');
-$sql = "SELECT * FROM users WHERE mail=? LIMIT 1"; // テーブル名を修正
+$pdo = new PDO('mysql:host=localhost;dbname=another_sky;charset=utf8', 'root', 'root');
+$sql = "SELECT * FROM users WHERE mail=? LIMIT 1"; 
 $ps = $pdo->prepare($sql);
 $ps->bindValue(1, $_POST['mail'], PDO::PARAM_STR);
 $ps->execute();
 $mailcheck = $ps->fetch();
-if ($mailcheck > 0) {
+
+if ($mailcheck !== false) {
     ?>
     <script type='text/javascript'>
         alert('既に同じメールアドレスが使用されています');
@@ -13,13 +14,13 @@ if ($mailcheck > 0) {
     </script>
 <?php
 } else {
-    $sql = "INSERT INTO users(mail, password, name) VALUES(?,?,?)"; // テーブル名を修正
+    $sql = "INSERT INTO users(mail, password, name) VALUES(?,?,?)";
     $ps = $pdo->prepare($sql);
     $ps->bindValue(1, $_POST['mail'], PDO::PARAM_STR);
     $ps->bindValue(2, password_hash($_POST['password'], PASSWORD_DEFAULT), PDO::PARAM_STR);
     $ps->bindValue(3, $_POST['name'], PDO::PARAM_STR);
     $ps->execute();
 
-    header('Location: account.php'); // リダイレクト先を修正
+    header('Location: user.php'); // リダイレクト先を修正
 }
 ?>
