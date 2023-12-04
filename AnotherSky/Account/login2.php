6ffd8665dab9e45fc0ff53c0b8ceadbffff4678d
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-12-04 14:40:37 +0900

    up

--- AnotherSky/Account/login2.php
+++ AnotherSky/Account/login2.php
@@ -9,7 +9,7 @@
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <meta http-equir="content-type" charset = "UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>lopgin</title>
 
@@ -18,87 +18,78 @@
         if(isset($_GET['message'])){
             $message = $_GET['message'];
         }
+        //セッションの情報が登録されている場合、グループログイン画面へと遷移する
         if(isset($_SESSION["mail"]) == true && isset($_SESSION["name"]) == true){
             header('Location: ../Top/menu.php');
         }
     ?>
-    </head>
+</head>
 
-    <style>
-        body {
-            background-color: #DDDDDD;
-        }
-        .form {
-            margin: 50px auto;
-            text-align: center;
-        }
-        .form_title {
-            font-size: 14px;
-            margin: 30px auto;
-        }
-        .label_left {
-            display: inline-block;
-            width: 10%;
-            border-left: solid 5px gray;
-            padding-left: 15px;
-            vertical-align: top;
-        }
-        input[type="email"],
-        input[type="password"] {
-            width: 500px;
-            padding: 10px;
-            background-color: white;
-        }
-        form .btn {
-            display: inline-block;
-            vertical-align: middle;
-            margin: 0 10px;
-            padding: 6px 25px;
-            color: gray;
-            font-weight: bold;
-            letter-spacing: 0.5pt;
-            text-decoration: none;
-            background-color: #ffffff;
-            border: 1px solid gray;
-            cursor: pointer;
-            transition-duration: 0.3s;
-            -webkit-transition-duration: 0.3s;
-            -moz-transition-duration: 0.3s;
-            -o-transition-duration: 0.3s;
-            -ms-transition-duration: 0.3s;
-        } 
-        form .btn:hover {
-            color: #ffffff;
-            background-color: gray;
-        }
-    </style>
-    <?php  require_once '../!Mng/header.php' ?>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Sacramento&display=swap');
+    body{
+        text-align: center;
+        background-color: #DDDDDD;
+        font-family: 'Sacramento', cursive;
+    } 
+    h1{
+        margin-bottom: 10%;
+        font-family: 'Sacramento', cursive;
+    }
+    h2 {     
+        text-align:center;
+        font-size : 25px; 
+        line-height : 5; 
+    }
+    button {    
+        padding: 1% 2%;
+        width: 600x;
+        border-radius: 10px;
+        border: 2px solid black;
+        background-color:	#C0C0C0;
+        font-family: 'Sacramento', cursive;
+    }
+    input{
+        padding: 1% 2%;
+        width: 600x;
+        border-radius: 10px;
+        border: 2px solid black;
+        background-color:	#C0C0C0;
+    } 
+    .test{
+        font-family: 'Sacramento', cursive;
+    }
+    p{
+    }
+</style>
 
-    <body>
-        <form action="login_check.php" method="post">
+<body>
+    <form action="login_check.php" method="post">
         <br><br>
 
-        <div class="form">
-        <div class="form_title">
-            <h1>LOGIN</h1>
-        </div>
-        <div class="form_area">
-            <form action="login_check.php" method="POST">
-                <label class="label_left" for="mail">メールアドレス:</label>
-                <input type="email" id="mail" name="mail" placeholder="e-mail" required><br><br>
+        <div id="error-message">
+            <div class="container mt-3"></div>
 
-                <label class="label_left" for="password">パスワード:</label>
-                <input type="password" id="password" name="pass" placeholder="pass" required><br><br>
+            <h1>Login</h1>
 
-                <input type="submit" name="login" value="ログイン" class="btn">
-            </form>
-        </div>
+            <div class="container mt-3">
+                <input type="text" class="from-control" name="mail" placeholder="mailaddress"><br><br>
+            </div>
+
+            <div class="container mt-3">
+                <input type="password" class="from-control" name="pass" placeholder="password"><br>
+            </div>
+            <br>
+            <a href="account.php">Sign up</a>
+            <br>
 
             <?php
                 if(isset($message)){
                     echo '<p class="error-message">ログインに失敗しました。正しい情報を入力してください</p>';
                 }
             ?>
+            <br>
+            <button type = "submit">Login</button>
         </div>
     </form>
 
@@ -132,6 +123,21 @@
     }
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<?php  require_once '../!Mng/footer.php' ?>
+<!--
+<script>
+var loginForm = document.querySelector("form");
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    var mailInput = document.querySelector('input[name="mail"]');
+    var passInput = document.querySelector('input[name="pass"]');
+    var errorContainer = document.getElementById("error-container");
+    if (mailInput.value === "" || passInput.value === "") {
+        errorContainer.textContent = "正しいメールアドレスとパスワードを入力してください。";
+    } else {
+        loginForm.submit();
+    }
+});
+</script>
+!-->
 </html>
 </body>
