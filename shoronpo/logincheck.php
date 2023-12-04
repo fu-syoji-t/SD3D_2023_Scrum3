16e0e33a0a824b7ef6ba0f3f8635ce3f3f54f26d
中村遥陽 <117244277+nakamura2101187@users.noreply.github.com>
2023-12-04 13:53:58 +0900

    up

--- shoronpo/logincheck.php
+++ shoronpo/logincheck.php
@@ -1,25 +1,27 @@
 <?php
 session_start();
 
-$pdo = new PDO('mysql:host=localhost;dbname=another_sky;charset=utf8', 'root', 'root');
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (isset($_POST['mail']) || isset($_POST['password'])) {
     $mail = $_POST['mail'];
-    $password = $_POST['pass'];
+    $password = $_POST['password'];
+
+    $pdo = new PDO('mysql:host=localhost;dbname=another_sky;charset=utf8', 'root', 'root');
 
     $sql = "SELECT * FROM users WHERE mail=? LIMIT 1";
     $ps = $pdo->prepare($sql);
     $ps->bindValue(1, $mail, PDO::PARAM_STR);
     $ps->execute();
     $user = $ps->fetch();
-
-    if ($user && password_verify($password, $user['password'])) {
-        // ログイン成功
+echo $password;
+echo '-----';
+echo $user;
+    if (password_verify($password, $user)) { 
         $_SESSION['user_id'] = $user['id']; 
         header('Location: account.php');
     } else {
-        // ログイン失敗
         echo '<script>alert("メールアドレスまたはパスワードが正しくありません");</script>';
     }
+} else {
+    echo '<script>alert("メールアドレスまたはパスワードが提供されていません");</script>';
 }
 ?>
--- shoronpo/toroku.php
+++ shoronpo/toroku.php
@@ -10,13 +10,14 @@ if ($mailcheck !== false) {
     ?>
     <script type='text/javascript'>
         alert('既に同じメールアドレスが使用されています');
-        location.href = 'toroku.html'; // ページ名を修正
+        location.href = 'toroku.php'; // ページ名を修正
     </script>
 <?php
 } else {
     $sql = "INSERT INTO users(mail, password, name) VALUES(?,?,?)";
     $ps = $pdo->prepare($sql);
     $ps->bindValue(1, $_POST['mail'], PDO::PARAM_STR);
+    //$ps->bindValue(2, $_POST['password'], PDO::PARAM_STR);
     $ps->bindValue(2, password_hash($_POST['password'], PASSWORD_DEFAULT), PDO::PARAM_STR);
     $ps->bindValue(3, $_POST['name'], PDO::PARAM_STR);
     $ps->execute();
